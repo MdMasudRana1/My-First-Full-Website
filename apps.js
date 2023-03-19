1ba8b35730c042964347a0a131a7056a8069920e
Md Masud Rana <121937277+MdMasudRana1@users.noreply.github.com>
2023-03-20 01:55:30 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,7 @@
+let navBar = document.querySelector (".menu");
+let bars = document.querySelector (".mobile-menu");
+
+bars.addEventListener("click", ()=>{
+      navBar.classList.toggle('close')
+})
+
